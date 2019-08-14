dc177919b79d7f7175944b5418f1ce804f313b73
Laxman Chamlagai <laxmanchamlagai20@gmail.com>
2019-08-13 22:23:51 -0400

    Unit converter functionality is added, final commit

---
+++ UnitConverter/src/com/JavaLearning/Main.java
@@ -0,0 +1,11 @@
+package com.JavaLearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double unitInPound = 200;
+	    double unitInKilogram = unitInPound * 0.45359237d;
+
+        System.out.println(" Converted Kilogram = " + unitInKilogram  + " KG");
+    }
+}
